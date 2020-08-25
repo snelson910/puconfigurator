ccd424c560b993e96126258a4f7f8999e043f97d
Steven Nelson <steven.nelson910@gmail.com>
2020-08-25 16:28:15 -0500

    Changes to flow of selection and modification system

--- PowerUnitConfigurator/static/reservoirsupport/pumpmanual.js
+++ PowerUnitConfigurator/static/reservoirsupport/pumpmanual.js
@@ -23,6 +23,9 @@ $(document).ready(function(){
 */
 $(document).ready(function(){
     $("#pumpnumsubmit").click(function(){
+        $("#pumpnumsubmit").attr("disabled", "disabled");
+        $("#pumpnums").attr("disabled", "disabled");
+        $("#pumpnummodify").removeAttr("disabled","disabled");
         pumpnum = Number($("#pumpnums").val());
         max = pumpnum + 1;
         pumpcurrent ++;
@@ -35,6 +38,17 @@ $(document).ready(function(){
             console.log("Complete");
         }
     })
+});
+$(document).ready(function(){
+    $("#pumpnummodify").click(function(){
+        if(confirm("Are you sure? This will remove all later selections.")){
+            //Will probably modify the "modify/confirm" buttons to simply lock each section as it goe. Not sure if I want a warning after each modifications 
+            //about potential deletions, because I don't want to go on forever coding this thing.
+
+        }else{
+
+        }
+    });
 });
 function pumps(){
     $.ajax({
@@ -61,12 +75,14 @@ function pumps(){
 function table(){
     if(pumpcurrent < max){
         var i = data.length;
-        $("#pumpconfiguration").append("<tr class='pumpnumber" + pumpcurrent + "' id='pumpnum" + pumpcurrent + "'><td class='pumpnumber" + pumpcurrent + "'>Select Pump " + pumpcurrent + "</td><td class='pumpnumber" + pumpcurrent + "'><select id='pump" + pumpcurrent + "' name='pump" + pumpcurrent + "'>");
+        $("#pumpconfiguration").append("<tr class='pumpnumber" + pumpcurrent + "' id='pumpnum" + pumpcurrent + "'><td class='pumpnumber" + pumpcurrent + "'>Select Pump " + pumpcurrent + 
+        "</td><td class='pumpnumber" + pumpcurrent + "'><select id='pump" + pumpcurrent + "' name='pump" + pumpcurrent + "'>");
         //This builds the selection option, but it is a little clunky. Can't figure out how to do it in django.forms with the joins that I need to run.
         for(j = 0; j < i; j++){
             $("#pump" + pumpcurrent).append("<option value='" + data[j] + "'>" + data[j] + "</option>");
         }
-        $("#pumpnum" + pumpcurrent).append("</select></td><td class='pumpnumber" + pumpcurrent + "'><button type='button' id='pump" + pumpcurrent + "submit' class='buttons' onclick='pumpsubmit(" + pumpcurrent + ")'>Submit Pump " + pumpcurrent + "</button></td></tr>");
+        $("#pumpnum" + pumpcurrent).append("</select></td><td class='pumpnumber" + pumpcurrent + "'><button type='button' id='pump" + pumpcurrent + "submit' class='buttons' onclick='pumpsubmit(" + 
+        pumpcurrent + ")'>Submit Pump " + pumpcurrent + "</button></td><td><button type='button' id='modify" + pumpcurrent + "' onclick='modify(" + pumpcurrent + ")' disabled='disabled'>Modify</button></td></tr>");
     }else{
         console.log("Completed");
     }
@@ -86,10 +102,29 @@ function pumpsubmit(number){
             console.log("Finished");
             $("button").attr("disabled", "disabled");
             $("select").attr("disabled", "disabled");
+            $(".modify").removeAttr("hidden", "hidden");
+            $(".modify").removeAttr("disabled", "disabled");
         }else{
             pumps();
         }
 }
+
+$(document).ready(function(){
+    $("#modify").click(function(){
+        $("button").removeAttr("disabled", "disabled");
+        $("select").removeAttr("disabled", "disabled");
+        $(".modify").attr("hidden", "hidden");
+        $(".modify").attr("disabled", "disabled");
+    });
+});
+
+
+
+
+
+
+
+
 
 var modal;
 var img;
--- newunit/views.py
+++ newunit/views.py
@@ -143,6 +143,7 @@ def pumps(request):
                      pumpcurrent = int(request.POST["pumpcurrent"])
                      selected = request.POST["selected"]
                      pumpcodes = []
+                     
                      if selected != "":
                             rearupper = Pumpcodes.objects.get(pump = selected)
                             data1 = rearupper.front_pump
@@ -160,14 +161,15 @@ def pumps(request):
                                    for x in data:
                                           pumpcodes.append(data[y].pump)
                                           y += 1
-                     else:
-                            
+                     else: 
                             query = 'select * from pumpcodes join throughdrives on throughdrives.rear_pump = pumpcodes.rear_pump where throughdrives.' + frontpump + ' is not null order by pump_class, pump_size'
                             data = Pumpcodes.objects.raw(query)
                             y = 0
                             for x in data:
                                    pumpcodes.append(data[y].pump)
                                    y += 1
+                     if len(pumpcodes) == 0:
+                            pumpcodes.append("Please choose a different forward pump")
                      jsondata = json.dumps(pumpcodes)
                      return HttpResponse(jsondata, content_type="application/json")
               else:
